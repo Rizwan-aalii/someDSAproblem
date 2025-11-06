67d3a3a000fa59138c5527b3dcb943c0dbd08e49
Rizwan-aalii <mrn892301@gmail.com>
2025-11-06 21:38:07 +0530

    linear search

--- Foundation_Problem's/Linear_Search.cpp
+++ Foundation_Problem's/Linear_Search.cpp
@@ -3,6 +3,22 @@
 
 using std :: string ;
 
+void printarray(int arr[] , int size )
+    {
+       std :: cout << "[" ;
+        for (int  i = 0; i < size; i++)
+        {
+           std ::  cout << arr[i]  ; 
+            if (i < size - 1 )
+            {
+               std :: cout << "," ;
+            }
+            
+        } 
+        std ::cout << "] \n   " ; 
+        
+    }
+
 bool findXinArray(int arrr[] , int x , int size )
 {
      bool found = false ;
@@ -13,12 +29,39 @@ bool findXinArray(int arrr[] , int x , int size )
         {
             std::cout <<"Value found : " << arrr[i] << " At Index : " << i << "\n";
             found = true ;
-        }
+        }  
      }
+     if (!found  == true)
+     {
+        std::cout <<"Value Not found At Array " << "\n";
+            found = false ;
+     }
+     
      return found ;
 
      
 }
+void findXinArray(int arrr[] , int x , int size , int value )
+{
+   bool found = false ;
+     for (int i = 0; i < size; i++)
+     {
+        if (arrr[i] == x)
+        {
+            arrr[i] = value ;
+            found = true ;
+            std::cout <<"Value found : " << arrr[i] << " At Index : " << i << " and Change by : " << value << "\n";
+        }  
+     }
+     if (!found  == true)
+     {
+        std::cout <<"Value Not found At Array " << "\n";
+     }
+     printarray(arrr , size);
+          
+}
+
+
 
 string intbool(bool value)
 {
@@ -37,9 +80,8 @@ int main ()
 {
 
     int size = 5 ;
-    int arr[size] = {5, 10, 15, 20, 25};
-    int x = 20 ;
-    bool found = findXinArray(arr , x , size);
-    string result = intbool(found);
-    std ::cout << result << "\n" ;
+    int arr[size] = {5, 15, 15, 20, 25};
+    int x = 15 ;
+    int change = 99 ;
+    findXinArray(arr , x , size , change);
 }
